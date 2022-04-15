722218548dfeeb8cb0662ae65fbce7cd0dd6c695
teacherSsamko <ssamko@kakao.com>
2022-04-15 14:25:23 +0900

    dynamodb: rmv range_key

--- data-storage/dyn/dynamodb.tf
+++ data-storage/dyn/dynamodb.tf
@@ -2,15 +2,9 @@ resource "aws_dynamodb_table" "schedule" {
   name         = "comroom_schedule"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "school_id"
-  range_key    = "hour"
 
   attribute {
     name = "school_id"
-    type = "N"
-  }
-
-  attribute {
-    name = "hour"
     type = "N"
   }
 
