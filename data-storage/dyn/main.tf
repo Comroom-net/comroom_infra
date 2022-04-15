5f212d4d4f44c52e4be1e0bdabb86906d198ea53
teacherSsamko <ssamko@kakao.com>
2022-04-15 14:19:21 +0900

    data-storage: dynamodb: innit

---
+++ data-storage/dyn/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.63.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    bucket         = "comroom-terraform-state"
+    key            = "prod/db/dynamodb/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "comroom-terraform-state-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+
